a7f008c26e617b6e6e178425a47718c204703436
Romildo A. Lima Jr. <odlimor.sevla@gmail.com>
2024-10-19 11:46:21 -0300

    Criando os cards dinamicamente

---
+++ src/scripts/engine.js
@@ -0,0 +1,28 @@
+const emojis = [
+    "☢",
+    "☢",
+    "✈",
+    "✈",
+    "🍔",
+    "🍔",
+    "🍺",
+    "🍺",
+    "🎪",
+    "🎪",
+    "🧛‍♀️",
+    "🧛‍♀️",
+    "👺",
+    "👺",
+    "🦝",
+    "🦝",
+];
+let openCard = [];
+
+let shuffleEmojis = emojis.sort(() => (Math.random() > 0.5 ? 2 : -1));
+
+for(let i = 0; i < emojis.length; i++){
+    let box = document.createElement("div");
+    box.className = "item";
+    box.innerHTML = shuffleEmojis[i];
+    document.querySelector(".game").appendChild(box);
+}
