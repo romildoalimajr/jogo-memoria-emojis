4333c074e5ca0e41bc65dc99f42c558bddba8554
Romildo A. Lima Jr. <odlimor.sevla@gmail.com>
2024-10-19 12:10:58 -0300

    Implementando o efeito de ROTATE nos cards.

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -16,7 +16,7 @@ const emojis = [
     "🦝",
     "🦝",
 ];
-let openCard = [];
+let openCards = [];
 
 let shuffleEmojis = emojis.sort(() => (Math.random() > 0.5 ? 2 : -1));
 
@@ -24,5 +24,18 @@ for(let i = 0; i < emojis.length; i++){
     let box = document.createElement("div");
     box.className = "item";
     box.innerHTML = shuffleEmojis[i];
+    box.onclick = handleClick;
     document.querySelector(".game").appendChild(box);
 }
+
+function handleClick(){
+    if(openCards.length < 2){
+        this.classList.add("boxOpen");
+        openCards.push(this);
+    }
+    if(openCards.length == 2){
+        setTimeout(checkMatch, 500);
+    }
+}
+
+function checkMatch(){}
